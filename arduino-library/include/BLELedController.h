e282c1d1c471490dacdde798044331cde7a4c744
Tirus <tirus@mail.com>
2023-12-30 19:44:02 +0100

    Arduino library: Fixed mixing of tabs and spaces.

--- arduino-library/include/BLELedController.h
+++ arduino-library/include/BLELedController.h
@@ -11,34 +11,34 @@ class BLEDevice;
 class BLECharacteristic;
 
 class BLELedController {
-    private:
-        struct LedMappingData;
+	private:
+		struct LedMappingData;
 
-        std::function<void(const char* mac)> onConnectCallback;
-        std::function<void(const char* mac)> onDisconnectCallback;
+		std::function<void(const char* mac)> onConnectCallback;
+		std::function<void(const char* mac)> onDisconnectCallback;
 
-        std::map<UUID, LedMappingData> uuidToCharacteristicMap;
+		std::map<UUID, LedMappingData> uuidToCharacteristicMap;
 
-        void onCharacteristicWritten(BLEDevice device, BLECharacteristic characteristic);
+		void onCharacteristicWritten(BLEDevice device, BLECharacteristic characteristic);
 
-        void handleLedInfoRequest(BLECharacteristic& characteristic);
-        void writeLedInfoDataV1(BLECharacteristic& characteristic);
+		void handleLedInfoRequest(BLECharacteristic& characteristic);
+		void writeLedInfoDataV1(BLECharacteristic& characteristic);
 
-        static void OnCharacteristicWritten(BLEDevice device, BLECharacteristic characteristic);
-        static void OnConnect(BLEDevice bleDevice);
-        static void OnDisconnect(BLEDevice bleDevice);
+		static void OnCharacteristicWritten(BLEDevice device, BLECharacteristic characteristic);
+		static void OnConnect(BLEDevice bleDevice);
+		static void OnDisconnect(BLEDevice bleDevice);
 
-        static RGBW ExtractRGBW(const BLECharacteristic& characteristic);
+		static RGBW ExtractRGBW(const BLECharacteristic& characteristic);
 
-    public:
-        BLELedController(const char* deviceName, const char* modelName = nullptr);
-        ~BLELedController();
+	public:
+		BLELedController(const char* deviceName, const char* modelName = nullptr);
+		~BLELedController();
 
-        void begin();
+		void begin();
 
 		void addRGBWCharacteristic(const std::string& name, std::function<void(RGBW newColor)> callback);
 		void setOnConnectCallback(std::function<void()> onConnectCallback);
 		void setOnDisconnectCallback(std::function<void()> onDisconnectCallback);
 
-        void update();
+		void update();
 };
